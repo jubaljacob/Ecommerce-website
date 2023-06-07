36834057f6444eaa3647f0707ad5b3dd5f7374d0
Jubal Jacob <jubalchandy@gmail.com>
2023-06-07 13:27:00 +0530

    .

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,18 @@ import { NavLink,Link } from 'react-router-dom'
 const Header = () => {
   return (
     <>
-    <header className='header-top-strip'>
+    <header className='header-top-strip py-3'>
       <div className='container-xxl'>
-        
+        <div className='row'>
+          <div className='col-6'></div>
+          <div className='col-6'></div>
+
+        </div>
       </div>
     </header>
     </>
   )
 }
+"emmet.includeLanguages": { "javascript": "javascriptreact" }
 
 export default Header
