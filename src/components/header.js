fe9ab2e9f5f350c567a523c03197cb8d6144fc99
Jubal Jacob <jubalchandy@gmail.com>
2023-06-07 13:35:53 +0530

    .

--- src/components/header.js
+++ src/components/header.js
@@ -6,15 +6,18 @@ const Header = () => {
     <header className='header-top-strip py-3'>
       <div className='container-xxl'>
         <div className='row'>
-          <div className='col-6'></div>
-          <div className='col-6'></div>
-
+          <div className='col-6'>
+            <p>Free shipping all Over Rs.1000/-</p>
+          </div>
+          <div className='col-6'>
+            <p className="text-end"></p>
+            <p>Hotline: <a href="tel:+91 67893456">+91 67893456</a> </p> 
+          </div>
         </div>
       </div>
     </header>
     </>
   )
 }
-"emmet.includeLanguages": { "javascript": "javascriptreact" }
 
 export default Header
