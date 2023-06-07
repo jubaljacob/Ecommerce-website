377206e0ece9f680106a14eb93204237c6c25e96
Jubal Jacob <jubalchandy@gmail.com>
2023-06-07 11:59:13 +0530

    about , contact added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import { BrowserRouter,Routes,Route} from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contatc from './pages/Contact';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Layout /> }>
           <Route index element = {<Home /> }/>
+          <Route path="about" element={ <About /> } />
+          <Route path="contact" element={ <Contatc /> } />
+          
         </Route>
       </Routes>
     </BrowserRouter>
