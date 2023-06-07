2f196eda7e0a43cbfcc6050c183dec952a13f0aa
joeljojo <81342970+Joel-Jojo@users.noreply.github.com>
2023-06-07 14:41:42 +0530

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,32 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <footer className="py-3"></footer>
+        <footer className="py-4">
+            <div className="container-xxl">
+                <div className="row align-itmes-center">
+                    <div className="col-5">
+                        <div className="footer-top-data d-flex gap 30 align-items-center">
+                            <img src="images/newsletter.png" alt="newletter" />
+                            <h2 className="mb=0 text-white">Sign Up for Newsletter</h2>
+                        </div>
+                    </div>
+                    <div className="col-7">
+                        <div className="input-group">
+                            <input
+                                type="text"
+                                className="form-control py-1"
+                                placeholder="Your Email Address"
+                                aria-label="Your Email Address"
+                                aria-aria-describedby="basic-addon2"
+                            />
+                            <span className="input-group-text p-2"  id="basic-addon2">
+                                Subscribe
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
         <footer className="py-3"></footer>
         <footer className="py-4">
             <div className="container-xxl">
