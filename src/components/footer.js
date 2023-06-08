88b660813167a2e1589413a2ade8f25fa219eda3
joeljojo <81342970+Joel-Jojo@users.noreply.github.com>
2023-06-08 10:24:21 +0530

    img

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import newsletter from "../images/newsletter.png";
+
 const Footer = () => {
   return (
     <>
@@ -7,7 +9,7 @@ const Footer = () => {
                 <div className="row align-itmes-center">
                     <div className="col-5">
                         <div className="footer-top-data d-flex gap 30 align-items-center">
-                            <img src="images/newsletter.png" alt="newletter" />
+                            <img src={newsletter} alt="newletter" />
                             <h2 className="mb=0 text-white">Sign Up for Newsletter</h2>
                         </div>
                     </div>
@@ -34,7 +36,7 @@ const Footer = () => {
                 <div className="row">
                     <div className="col-12">
                        <p className="text-center mb-0 text-white ">
-                        &copy; {new Date().getFullYear()}; Powered by Developer's Corner 
+                        &copy; {new Date().getFullYear()}; Powered by 
                         </p> 
                     </div>
                 </div>
