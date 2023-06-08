5038eafdc364228d1bad4eb547b5f6ff5e5bfe9c
Jubal Jacob <jubalchandy@gmail.com>
2023-06-08 11:21:40 +0530

    main banner

--- src/components/header.js
+++ src/components/header.js
@@ -33,7 +33,7 @@ const Header = () => {
           <div className="row align-items-center">
             <div className="col-2">
               <h2>
-                <Link className="text-white">Dev Corner</Link>
+                <Link className="text-white">Jojo's</Link>
               </h2>
             </div>
             <div className="col-5">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import mainbanner from '../images/main-banner-1.jpg'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <>
+  <section className="home-wrapper-1 py-5">
+    <div className="container-xxl">
+      <div className="row">
+        <div className="cl-6">
+          <div className="main-banner position-relative p-3">
+            <img src={mainbanner} className='img-fluid rounded-3' 
+            alt="main-banner" 
+            />
+            <div className="main-banner-content position-absolute">
+              <h4>Era of spacial computing</h4>
+              <h5>Apple Vision Pro.</h5>
+              <p>From $3499 onwards.</p>
+              <Link className='button'>Buy now</Link>
+            </div>
+          </div>
+        </div>
+        <div className="cl-6"></div>
+      </div>
+    </div>
+  </section>
+  </>
 }
 
 export default Home
