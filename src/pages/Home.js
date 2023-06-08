bee141e6c1d1f0ee379b1aeefb2c0ce567af3ac9
Jubal Jacob <jubalchandy@gmail.com>
2023-06-08 11:53:18 +0530

    main-banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,95 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import mainbanner from '../images/main-banner-1.jpg'
+import React from 'react';
+import {Link} from "react-router-dom";
+import mainbanner from '../images/main-banner-1.jpg';
+import catbanner1 from '../images/catbanner-01.jpg';
+import catbanner2 from '../images/catbanner-02.jpg';
+import catbanner3 from '../images/catbanner-03.jpg';
+import catbanner4 from '../images/catbanner-04.jpg'
+
+
+
 
 const Home = () => {
   return <>
   <section className="home-wrapper-1 py-5">
     <div className="container-xxl">
-      <div className="row">
-        <div className="cl-6">
-          <div className="main-banner position-relative p-3">
-            <img src={mainbanner} className='img-fluid rounded-3' 
-            alt="main-banner" 
-            />
-            <div className="main-banner-content position-absolute">
-              <h4>Era of spacial computing</h4>
-              <h5>Apple Vision Pro.</h5>
-              <p>From $3499 onwards.</p>
-              <Link className='button'>Buy now</Link>
+    <div className="row">
+          <div className="col-6">
+          <div className="main-banner position-relative ">
+              <img
+                src={mainbanner}
+                className="img-fluid rounded-3"
+                alt="main banner"
+              />
+              <div className="main-banner-content position-absolute">
+                <h4>Era of spacial computing</h4>
+                <h5>Vision Pro.</h5>
+                <p>From $3499</p>
+                <Link className="button">BUY NOW</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+              <div className="small-banner position-relative">
+                <img
+                  src={catbanner1}
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>Best Sake</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+              <div className="small-banner position-relative">
+                <img
+                  src={catbanner2}
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>But IPad Air</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+              <div className="small-banner position-relative ">
+                <img
+                  src={catbanner3}
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>But IPad Air</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+              <div className="small-banner position-relative ">
+                <img
+                  src={catbanner4}
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>But IPad Air</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="cl-6"></div>
-      </div>
     </div>
   </section>
   </>
