3f7f1a85925be1d961cec0579d658791157aaf67
Jubal Jacob <jubalchandy@gmail.com>
2023-06-13 13:49:56 +0530

    .

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import OurStore from './pages/OurStore';
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +16,7 @@ function App() {
           <Route index element = {<Home /> }/>
           <Route path="about" element={ <About /> } />
           <Route path="contact" element={ <Contact /> } />
+          <Route path="product" element={<OurStore />} />
           
         </Route>
       </Routes>
