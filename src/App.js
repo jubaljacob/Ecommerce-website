914cac9b0c87f5b586629638a92e5dc1c53e7eb2
joeljojo <81342970+Joel-Jojo@users.noreply.github.com>
2023-06-16 14:54:26 +0530

    .

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import OurStore from "./pages/OurStore";
+import OurStore from "./pages/Store";
 import Blog from "./pages/Blog";
 import CompareProducts from "./pages/CompareProducts";
 import Wishlist from "./pages/Wishlist";
@@ -30,7 +30,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
-            <Route path="OurStore" element={<OurStore />} />
+            <Route path="Store" element={<Store />} />
             <Route path="compare-product" element={<CompareProducts />} />
             <Route path="wishlist" element={<Wishlist />} />
             <Route path="login" element={<Login />} />
---
+++ src/pages/Store.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import Meta from "../components/Meta";
+import ReactStars from "react-rating-stars-component";
+import ProductCard from "../components/ProductCard";
+import Color from "../components/Color";
+import Container from "../components/Container";
+
+const Store = () => {
+  const [grid, setGrid] = useState(4);
+  return (
+    <>
+      <Meta title={"Store"} />
+      <Container class1="store-wrapper home-wrapper-2 py-5">
+        <div className="row">
+          <div className="col-3">
+            <div className="filter-card mb-3">
+              <h3 className="filter-title">Shop By Categories</h3>
+              <div>
+                <ul className="ps-0">
+                  <li>Watch</li>
+                  <li>Tv</li>
+                  <li>Camera</li>
+                  <li>Laptop</li>
+                </ul>
+              </div>
+            </div>
+            <div className="filter-card mb-3">
+              <h3 className="filter-title">Filter By</h3>
+              <div>
+                <h5 className="sub-title">Availablity</h5>
+                <div>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id=""
+                    />
+                    <label className="form-check-label" htmlFor="">
+                      In Stock (1)
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id=""
+                    />
+                    <label className="form-check-label" htmlFor="">
+                      Out of Stock(0)
+                    </label>
+                  </div>
+                </div>
+                <h5 className="sub-title">Price</h5>
+                <div className="d-flex align-items-center gap-10">
+                  <div className="form-floating">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="floatingInput"
+                      placeholder="From"
+                    />
+                    <label htmlFor="floatingInput">From</label>
+                  </div>
+                  <div className="form-floating">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="floatingInput1"
+                      placeholder="To"
+                    />
+                    <label htmlFor="floatingInput1">To</label>
+                  </div>
+                </div>
+                <h5 className="sub-title">Colors</h5>
+                <div>
+                  <Color />
+                </div>
+                <h5 className="sub-title">Size</h5>
+                <div>
+                  <div className="form-check">
+                    <input 
+                      className="form-check-label" 
+                      value=""
+                      id="color-1"
+                    />
+                  </div>
+                  <label className="form-check-label" htmlFor="color-1">
+                    s (2)
+                  </label>
+                </div>
+                <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="color-2"
+                    />
+                    <label className="form-check-label" htmlFor="color-2">
+                    M (2)
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </Container>
+    </>
+    );
+  };
+
+            
+
+export default Store;
